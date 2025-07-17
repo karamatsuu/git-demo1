676facc3179f4a6e5f17d25a3716aa356d05dc93
karamatsu <karamatabatbaev4@gmail.com>
2025-07-17 20:11:55 +0500

    First java file

---
+++ first.java
@@ -0,0 +1,13 @@
+public class first {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Juice World\n");
+        System.out.print("Karamat");
+        //Comment
+        /*Comment 
+        lines */
+        /*
+        sout-is shortcut for System.out.println();
+        */
+    }    
+}
